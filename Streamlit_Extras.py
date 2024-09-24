0d96e3bc93a77db7f325c28c048fa645ced3dd4f
Darxke <shixuanzhu123@gmail.com>
2024-09-23 23:00:57 -0700

    Moved coffee button

--- Streamlit_Extras.py
+++ Streamlit_Extras.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from streamlit_extras.buy_me_a_coffee import button
 from streamlit_extras.let_it_rain import rain
+from streamlit.components.v1 import html
+
 def coffee():
-    button = """
-    <script type="text/javascript" src="https://buymeacoffee.com/shixuanzhuk" data-name="bmc-button" data-slug="adof6qkj3h" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
-    """
+    with st.sidebar:
+        button(username="shixuanzhuk", floating=False, width=221)
+
+
 def rainEmojis(emoji):
     rain(
         emoji=emoji,
