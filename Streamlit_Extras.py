f76b5e350628da38150a3e347a65271b6853ef60
Darxke <shixuanzhu123@gmail.com>
2024-09-23 22:31:19 -0700

    Update Streamlit_Extras.py

--- Streamlit_Extras.py
+++ Streamlit_Extras.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_extras.buy_me_a_coffee import button
 from streamlit_extras.let_it_rain import rain
 def coffee():
-    button(username="fake-username", floating=False, width=221)
+    button(username="https://buymeacoffee.com/shixuanzhuk/", floating=False, width=221)
 def rainEmojis(emoji):
     rain(
         emoji=emoji,
