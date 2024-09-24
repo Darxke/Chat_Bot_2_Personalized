ac4dedada3a49e78b3989ab7af669e19dd29a913
Darxke <shixuanzhu123@gmail.com>
2024-09-23 22:32:29 -0700

    Update Streamlit_Extras.py

--- Streamlit_Extras.py
+++ Streamlit_Extras.py
@@ -2,7 +2,22 @@ import streamlit as st
 from streamlit_extras.buy_me_a_coffee import button
 from streamlit_extras.let_it_rain import rain
 def coffee():
-    button(username="https://buymeacoffee.com/shixuanzhuk/", floating=False, width=221)
+    button = """
+    <script type="text/javascript" src="https://buymeacoffee.com/shixuanzhuk" data-name="bmc-button" data-slug="adof6qkj3h" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+    """
+
+    st.markdown(
+        """
+        <style>
+            iframe[width="220"] {
+                position: fixed;
+                bottom: 60px;
+                right: 40px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 def rainEmojis(emoji):
     rain(
         emoji=emoji,
